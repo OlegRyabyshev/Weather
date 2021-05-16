df5f8f3b7a3420bceab968f298d9700c3a032d2a
Oleg Ryabyshev <oryabyshev@gmail.com>
2021-05-16 23:18:12 +0300

    upd1

--- app/src/main/java/xyz/fcr/weather/MainActivity.kt
+++ app/src/main/java/xyz/fcr/weather/MainActivity.kt
@@ -2,10 +2,21 @@ package xyz.fcr.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.button.MaterialButton
+import 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val updateButton: MaterialButton = findViewById(R.id.)
+        val tempText: TextView
+        val
+
+        updateButton.setOnClickListener {
+
+        }
     }
 }
---
+++ app/src/main/java/xyz/fcr/weather/objects/WeatherInfo.kt
@@ -0,0 +1,3 @@
+package xyz.fcr.weather.objects
+
+data class WeatherInfo(val currentTemp: Int = 27, val currentPressure: Int = 700)
