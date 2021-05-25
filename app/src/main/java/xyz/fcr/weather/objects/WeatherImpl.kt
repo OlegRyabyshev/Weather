a8c73ea51915d2b4b99ad84989fd65665a3331c3
OlegRyabyshev <oryabyshev@gmail.com>
2021-05-25 09:38:38 +0300

    Logic & City chooser

--- app/src/main/java/xyz/fcr/weather/objects/WeatherImpl.kt
+++ app/src/main/java/xyz/fcr/weather/objects/WeatherImpl.kt
@@ -2,13 +2,6 @@ package xyz.fcr.weather.objects
 
 class WeatherImpl : WeatherInterface {
     override fun getWeatherData(): WeatherObject {
-        return WeatherObject(
-            "St. Peter",
-            "20",
-            "32",
-            "10",
-        "-20",
-            "Raining all day all night everyday"
-        )
+        return WeatherObject("New York City", "20", "32")
     }
 }
--- app/src/main/java/xyz/fcr/weather/objects/WeatherObject.kt
+++ app/src/main/java/xyz/fcr/weather/objects/WeatherObject.kt
@@ -6,20 +6,24 @@ import java.util.*
 
 data class WeatherObject(
     val cityName: String,
-    val cityTemp: String,
-    val cityHighestTemp: String,
-    val cityLowestTemp: String,
-    val cityFeelsLikeTemp: String,
-    val cityDescription: String,
-) {
-    init {
-        val cityDate: String = getCurrentDate()
-    }
+    val lat: String,
+    val lon: String,
+    val cityDate: String = getCurrentDate(),
+    val cityTemp: String = getCurrentTemp(),
+    val cityDescription: String = getDescription(),
+)
+
+fun getCurrentTemp(): String {
+    return "27"
+}
+
+fun getDescription(): String {
+    return "Mostly Cloudy"
 }
 
 @SuppressLint("SimpleDateFormat")
 fun getCurrentDate(): String {
-    val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
-    val currentDate = sdf.format(Date())
-    return ("Date$currentDate")
+    //val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+   //val currentDate = sdf.format(Date())
+    return ("Fri, 31 July 5:40dp")
 }
