298820d08696d1f4a08c9493ebcc3d83478b5d74
OlegRyabyshev <oryabyshev@gmail.com>
2021-06-03 16:57:55 +0300

    BroadcastReceiver impl

--- app/src/main/java/xyz/fcr/weather/MainActivity.kt
+++ app/src/main/java/xyz/fcr/weather/MainActivity.kt
@@ -1,10 +1,17 @@
 package xyz.fcr.weather
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import xyz.fcr.weather.broadcast.ConnectionBroadcastReceiver
 import xyz.fcr.weather.fragments.WeatherFragment
 
+
 class MainActivity : AppCompatActivity() {
+
+    private val receiver = ConnectionBroadcastReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +23,14 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, WeatherFragment())
                 .commit()
         }
+
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION)
+        registerReceiver(receiver, intentFilter)
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
     }
 }
---
+++ app/src/main/java/xyz/fcr/weather/broadcast/ConnectionBroadcastReceiver.kt
@@ -0,0 +1,17 @@
+package xyz.fcr.weather.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.widget.Toast
+
+class ConnectionBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        if (ConnectivityManager.CONNECTIVITY_ACTION == intent?.action) {
+            Toast.makeText(context,"Change in connection", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
